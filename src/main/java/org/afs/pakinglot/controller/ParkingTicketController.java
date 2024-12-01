3d9d770fee984db162bca330d5967e4258c231ad
Minghao Li <minghao.li@thoughtworks.com>
2024-12-01 22:52:01 +0800

    [ParkingLot][2.0] chore: remove controller

--- src/main/java/org/afs/pakinglot/controller/ParkingTicketController.java
+++
@@ -1,9 +0,0 @@
-package org.afs.pakinglot.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/parking-tickets")
-public class ParkingTicketController {
-}
