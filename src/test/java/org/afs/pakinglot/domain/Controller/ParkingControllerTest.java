8beb7d1638e7835b7e2493cc79f7bb77ad98d934
SHENZA <shenza@oocl.com>
2024-12-08 19:03:33 +0800

    test,test fetch api

--- src/test/java/org/afs/pakinglot/domain/Controller/ParkingControllerTest.java
+++ src/test/java/org/afs/pakinglot/domain/Controller/ParkingControllerTest.java
@@ -245,4 +245,78 @@ public class ParkingControllerTest {
         assertEquals(givenParkingLots.get(2).getId(), ticket3.getParkingLot().getId());
         assertEquals(givenParkingLots.get(1).getId(), ticket4.getParkingLot().getId());
     }
+
+    @Test
+    void should_return_car_when_fetch_given_ticket() throws Exception {
+        // Given
+        String givenPlateNumber = generatePlate();
+        String givenTicketDto = String.format(
+                "{\"plateNumber\": \"%s\"}",
+                givenPlateNumber
+        );
+        ticketRepository.save(new Ticket(givenPlateNumber, 1, parkingLotRepository.findAll().get(0)));
+
+        // When
+        // Then
+        String contentAsString = client.perform(MockMvcRequestBuilders.post("/parking/fetch")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(givenTicketDto))
+                .andReturn().getResponse().getContentAsString();
+        TicketVo ticketVo = ticketVoJacksonTester.parseObject(contentAsString);
+        AssertionsForClassTypes.assertThat(ticketVo.getPlateNumber()).isEqualTo(givenPlateNumber);
+    }
+
+    @Test
+    void should_return_nothing_with_error_message_when_fetch_given_an_unrecognized_ticket() throws Exception {
+        // Given
+        String givenPlateNumber = generatePlate();
+        String givenTicketDto = String.format(
+                "{\"plateNumber\": \"%s\"}",
+                givenPlateNumber
+        );
+
+        // When
+        // Then
+        try {
+            client.perform(MockMvcRequestBuilders.post("/parking/fetch")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(givenTicketDto))
+                    .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                    .andExpect(result -> {
+                        String errorMessage = result.getResponse().getContentAsString();
+                        assertTrue(errorMessage.contains("Unrecognized parking ticket."));
+                    });
+        } catch (Exception ignore) {
+        }
+    }
+
+    @Test
+    void should_return_nothing_with_error_message_when_fetch_given_an_used_ticket() throws Exception {
+        // Given
+        String givenPlateNumber = generatePlate();
+        String givenTicketDto = String.format(
+                "{\"plateNumber\": \"%s\"}",
+                givenPlateNumber
+        );
+        ticketRepository.save(new Ticket(givenPlateNumber, 1, parkingLotRepository.findAll().get(0)));
+
+        // When
+        client.perform(MockMvcRequestBuilders.post("/parking/fetch")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(givenTicketDto))
+                .andReturn().getResponse().getContentAsString();
+
+        // Then
+        try {
+            client.perform(MockMvcRequestBuilders.post("/parking/fetch")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(givenTicketDto))
+                    .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                    .andExpect(result -> {
+                        String errorMessage = result.getResponse().getContentAsString();
+                        assertTrue(errorMessage.contains("Unrecognized parking ticket."));
+                    });
+        } catch (Exception ignore) {
+        }
+    }
 }
