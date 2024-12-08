321c15c726628af1188182df932a0c3792b634d3
SHENZA <shenza@oocl.com>
2024-12-08 17:19:42 +0800

    Revert "feat,add ParkingManager"
    This reverts commit 5ecfb76a6aa18890c55ba01468e15efc532a8436.

--- src/main/java/org/afs/pakinglot/domain/Ticket.java
+++ src/main/java/org/afs/pakinglot/domain/Ticket.java
@@ -1,6 +1,4 @@
 package org.afs.pakinglot.domain;
 
-import java.time.LocalDateTime;
-
-public record Ticket(String plateNumber, int position, int parkingLot, LocalDateTime parkingTime) {
+public record Ticket(String plateNumber, int position, int parkingLot) {
 }
