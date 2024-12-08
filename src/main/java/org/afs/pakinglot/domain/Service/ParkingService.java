d7d0c7263129aad2f39d75a86f0faf29072979de
SHENZA <shenza@oocl.com>
2024-12-08 18:24:10 +0800

    feat,remove useless entity and test

--- src/main/java/org/afs/pakinglot/domain/Service/ParkingService.java
+++ src/main/java/org/afs/pakinglot/domain/Service/ParkingService.java
@@ -1,6 +1,5 @@
 package org.afs.pakinglot.domain.Service;
 
-import org.afs.pakinglot.domain.entity.ParkingBoy;
 import org.afs.pakinglot.domain.strategies.ParkingStrategy;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +9,8 @@ import java.util.Map;
 @Service
 public class ParkingService {
     private Map<String, ParkingStrategy> strategyMap;
-    private ParkingBoy parkingBoy;
-    public ParkingService(Map<String, ParkingStrategy> strategyMap, ParkingBoy parkingBoy) {
+    public ParkingService(Map<String, ParkingStrategy> strategyMap) {
         this.strategyMap = strategyMap;
-        this.parkingBoy = parkingBoy;
     }
     public List<String> getParkingStrategy() {
         return strategyMap.keySet().stream().toList();
